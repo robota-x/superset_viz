74420156667b0f18ef24467ce0e6076b3ba510a0
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-04-20 18:39:05 +0100

    add some local caching

--- fetch_monzo_data.py
+++ fetch_monzo_data.py
@@ -1,4 +1,5 @@
 import configparser
+import json
 import requests
 
 
@@ -7,7 +8,7 @@ API_ENDPOINT = "https://api.monzo.com"
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-def fetch_transaction_details():
+def fetch_transaction_list_from_api():
     account_id = config['MONZO']['account_id']
     auth_token = config['MONZO']['auth_token']
 
@@ -19,8 +20,19 @@ def fetch_transaction_details():
     return res.json()
 
 
-a = fetch_transaction_details()
-
-from pprint import pprint
-
-pprint(a)
+def load_transaction_list(update_cache=False):
+    try:
+        with open('data/transaction_list.json', 'r') as file:
+            print('Loading from cache!')
+            transaction_list = json.load(file)
+    except FileNotFoundError:
+        update_cache = True
+        print('Missing cache file!')
+    if update_cache:
+        print('Downloading transaction list...')
+        with open('data/transaction_list.json', 'x') as file:
+            transaction_list = fetch_transaction_list_from_api()
+            json.dump(transaction_list, file)
+        
+    
+load_transaction_list()
