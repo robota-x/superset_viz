d0f9fdbf184c7f406acd0d085456866753c02806
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-04-20 21:54:31 +0100

    use dateutil to input valid transaction date

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+from dateutil.parser import parse
+
 from monzo_interface.db_helpers import get_session, get_engine, get_or_create_by_id
 from monzo_interface.models import Merchant, Transaction
 
 
-
 fake_transaction = {
     "id": "tsegserserhX",
-    "created": "2018-12-10T09:22:08.45Z",
+    "created": parse("2018-12-10T09:22:08.45Z"),
     "description": "BBC MEDIA CITY SALFORD SALFORD GBR",
     "amount": -2150,
     "currency": "GBP",
 }
 
-id = fake_transaction.pop('id')
+
+# playing around
+id = fake_transaction.pop("id")
 a = get_or_create_by_id(Transaction, id, **fake_transaction)
 print(a)
