b95e458a10d727bc013c678c5b21e370e569b920
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-04-27 19:43:09 +0100

    allow javascript tooltips, dump spending dashboard

--- superset_config.py
+++ superset_config.py
@@ -7,3 +7,4 @@ MAPBOX_API_KEY = config["SUPERSET"]["mapbox_api_key"]
 SECRET_KEY = config["SUPERSET"]["secret_key"]
 
 SQLALCHEMY_DATABASE_URI = f"{config['DATABASE']['location']}_main"
+ENABLE_JAVASCRIPT_CONTROLS = True
